183acefbcee13697ada3ec97b1a1c4719572b8b8
Anny Hsu <annnnnyjj@gmail.com>
2016-02-09 11:29:47 +0000

    Add happyfoxchat

--- public_html/fancybox/source/jquery.fancybox.js
+++ public_html/fancybox/source/jquery.fancybox.js
@@ -2017,4 +2017,24 @@
 		$("<style type='text/css'>.fancybox-margin{margin-right:" + (w2 - w1) + "px;}</style>").appendTo("head");
 	});
 
+        $.video.click(function() {
+        	$.fancybox({
+                'padding'       : 0,
+                'autoScale'     : false,
+                'transitionIn'  : 'none',
+                'transitionOut' : 'none',
+                'title'         : this.title,
+                'width'     : 680,
+                'height'        : 495,
+                'href'          : this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
+                'type'          : 'swf',
+                'swf'           : {
+                'wmode'        : 'transparent',
+                'allowfullscreen'   : 'true'
+        		}
+        	});
+
+       		 return false;
+   		 });
+
 }(window, document, jQuery));
---
+++ public_html/js/happfoxchat.js
@@ -0,0 +1,20 @@
+    <!--Start of HappyFox Live Chat Script-->
+    
+    window.HFCHAT_CONFIG = {
+         EMBED_TOKEN: "544cca30-ced6-11e5-8f92-1b85d2156a63",
+         ACCESS_TOKEN: "22667820ee5e4f13bc58ee6bff217254",
+         HOST_URL: "https://happyfoxchat.com",
+         ASSETS_URL: "https://d1l7z5ofrj6ab8.cloudfront.net/visitor"
+     };
+
+    (function() {
+      var scriptTag = document.createElement('script');
+      scriptTag.type = 'text/javascript';
+      scriptTag.async = true;
+      scriptTag.src = window.HFCHAT_CONFIG.ASSETS_URL + '/js/widget-loader.js';
+
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(scriptTag, s);
+    })();
+
+    <!--End of HappyFox Live Chat Script-->
